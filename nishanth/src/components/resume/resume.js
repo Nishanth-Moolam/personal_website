8e45c1b1af044c287dbbdc3c57d7ea4f0ed34ce9
Nishanth <nishanthmoolam97@gmail.com>
2023-12-21 15:52:12 -0500

    fixed styling and resume scroll

--- nishanth/src/components/resume/resume.js
+++ nishanth/src/components/resume/resume.js
@@ -1,22 +1,23 @@
 import "./resume.css";
 
 const AppResume = () => {
+  document.body.classList.add("no-scroll");
+
   return (
     <div className="resume-block">
-      {/* <iframe
-        src="../../assets/Nishanth_Moolam.pdf"
-        title="resume"
-        width="100%"
-        height="500px"
-      /> */}
       <object
         data="/Nishanth_Moolam.pdf"
         type="application/pdf"
         className="resume"
       >
         <p>
-          Alternative text - include a link{" "}
-          <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a>
+          <a
+            href="/Nishanth_Moolam.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="resume-download-button">Download Resume</button>
+          </a>
         </p>
       </object>
     </div>
